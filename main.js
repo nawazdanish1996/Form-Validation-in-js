a770bb706b8e942f994fd64037b1268c03272532
Nawaz Danish <nawazdanish1996@gmail.com>
2022-09-15 02:42:52 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+// submit
+let loginBtn = document.getElementById("submitBtn");
+let password1 = document.getElementById("password1");
+let password2 = document.getElementById("password2");
+let username = document.getElementById("username");
+
+loginBtn.addEventListener("click", function (e) {
+    e.preventDefault()
+
+    
+    if(password1.value != password2.value){
+        alert("Invalid Input")
+    }
+    else if(password1.value.length && password2.value.length < 8){
+        alert("Password should be greater than 8");
+    }
+    else if(password1.value === "" && password2.value == "" || username.value == ""){
+        alert("Invalid Input");
+        console.log("Invalid Input");
+    }
+    else{
+        console.log("Login");
+        console.log("Password Verified " + password1.value + " " + password2.value);
+        console.log("Username:" + username.value);
+        password1.value ="";
+        password2.value ="";
+        username.value ="";
+    }
+})
